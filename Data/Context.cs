f1ff6b1c02d8ab118d5faae62f6c1ac3a5bef946
Nicolas Tesla <65693466+NicolasTeslaa@users.noreply.github.com>
2023-05-02 19:47:49 -0300

    atts

--- Data/Context.cs
+++ Data/Context.cs
@@ -24,9 +24,15 @@ namespace IdentityASPNet.Data
             modelBuilder.Entity<UserRole>(userRole =>
             {
                 userRole.HasKey(ur => new { ur.UserId, ur.RoleId });
+
                 userRole.HasOne(ur => ur.Role)
                                 .WithMany(r => r.UserRoles)
                                 .HasForeignKey(ur => ur.RoleId)
+                                .IsRequired();
+
+                userRole.HasOne(ur => ur.User)
+                                .WithMany(r => r.UserRoles)
+                                .HasForeignKey(ur => ur.UserId)
                                 .IsRequired();
             });
         }
