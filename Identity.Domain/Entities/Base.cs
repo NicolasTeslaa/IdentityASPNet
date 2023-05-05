13d6c0eb39b987603579f63e36232e350a62e14f
Nicolas Tesla <65693466+NicolasTeslaa@users.noreply.github.com>
2023-05-05 15:14:44 -0300

    atts

--- Identity.Domain/Entities/Base.cs
+++ Identity.Domain/Entities/Base.cs
@@ -1,9 +1,15 @@
+using System.Collections.Generic;
+
 namespace Identity.Domain.Entities
 {
     public abstract class Base
     {
         public long Id { get; set; }
 
-        
+        //gera uma lista de erros para que as entidades se auto validatem
+        internal List<string>? _errors;
+        // le as propriedades acima
+        public IReadOnlyCollection<string> Errors => _errors;
+        public abstract bool Validate();
     }
 }
