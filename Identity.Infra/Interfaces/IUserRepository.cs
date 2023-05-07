1e6d715b6cfb6ed7f99611db9f8accad460cb7cc
Nicolas Tesla <65693466+NicolasTeslaa@users.noreply.github.com>
2023-05-07 17:33:14 -0300

    atts

--- Identity.Infra/Interfaces/IUserRepository.cs
+++ Identity.Infra/Interfaces/IUserRepository.cs
@@ -5,6 +5,7 @@ namespace Identity.Infra.Interfaces
     public interface IUserRepository : IBaseRepository<User>
     {
          Task<User> GetByEmail (string email);
-         
+         Task<List<User>> SearchByEmail(string email);
+
     }
 }
