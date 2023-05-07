56f83d810cd3de134d68cc846d941f1ea7ccda69
Nicolas Tesla <65693466+NicolasTeslaa@users.noreply.github.com>
2023-05-07 17:34:36 -0300

    atts

--- Identity.Infra/Interfaces/IUserRepository.cs
+++ Identity.Infra/Interfaces/IUserRepository.cs
@@ -6,6 +6,6 @@ namespace Identity.Infra.Interfaces
     {
          Task<User> GetByEmail (string email);
          Task<List<User>> SearchByEmail(string email);
-
+        Task<List<User>> SearchByName(string name);
     }
 }
