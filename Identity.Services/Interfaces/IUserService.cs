5c134a1578e0f0ef6ebfd7a1ecab4f05b3b811a4
Nicolas Tesla <65693466+NicolasTeslaa@users.noreply.github.com>
2023-05-08 11:47:44 -0300

    atts

--- Identity.Services/Interfaces/IUserService.cs
+++ Identity.Services/Interfaces/IUserService.cs
@@ -11,6 +11,6 @@ namespace Identity.Services
         Task<List<UserDTO>> Get();
         Task<List<UserDTO>> SearchByName(string name);
         Task<List<UserDTO>> SearchByEmail(string email);
-
+        Task<UserDTO> GetByEmail(string email);
     }
 }
