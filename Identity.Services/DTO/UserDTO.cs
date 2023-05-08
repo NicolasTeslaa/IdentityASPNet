5f7083e9893e95a2a71e82a69af832508d943b3c
Nicolas Tesla <65693466+NicolasTeslaa@users.noreply.github.com>
2023-05-08 11:36:33 -0300

    atts

---
+++ Identity.Services/DTO/UserDTO.cs
@@ -0,0 +1,9 @@
+namespace Identity.Services.DTO
+{
+    public class UserDTO
+    {
+        public string Name { get;  set; }
+        public string Email { get;  set; }
+        public string Password { get;  set; }
+    }
+}
